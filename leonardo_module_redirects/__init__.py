d7c558c2b8226756b32c84badcbd5e6375baf72d
dresl <nikicresl@gmail.com>
2018-12-16 18:32:59 +0100

    Update

--- leonardo_module_redirects/__init__.py
+++ leonardo_module_redirects/__init__.py
@@ -9,10 +9,6 @@ LEONARDO_APPS = [
     'django.contrib.redirects',
 ]
 
-MIDDLEWARE_CLASSES = [
-    'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
-]
-
 class Config(AppConfig):
     name = 'leonardo_module_redirects'
     verbose_name = "leonardo-module-redirects"
