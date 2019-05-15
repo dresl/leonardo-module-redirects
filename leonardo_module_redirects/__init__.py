2e1322526de87b64a7acc4082a07248e06a000dd
Dominik Resl <info@resldominik.cz>
2019-05-15 23:49:15 +0200

    Add init due to new deploy

--- leonardo_module_redirects/__init__.py
+++ leonardo_module_redirects/__init__.py
@@ -3,11 +3,16 @@ from django.apps import AppConfig
 
 default_app_config = 'leonardo_module_redirects.Config'
 
+try:
+    from local_settings import APPS
+except ImportError:
+    pass
 
-LEONARDO_APPS = [
-    'leonardo_module_redirects',
-    'django.contrib.redirects',
-]
+if 'leonardo_module_redirects' in APPS:
+    LEONARDO_APPS = [
+        'leonardo_module_redirects',
+        'django.contrib.redirects',
+    ]
 
 class Config(AppConfig):
     name = 'leonardo_module_redirects'
